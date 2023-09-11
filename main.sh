c3c618dca2b34278817df6fc28e7a2e073bc1911
Ward Nakchbandi (Cosmic Fusion) <83735213+CosmicFusion@users.noreply.github.com>
2023-09-11 22:08:00 +0300

    Update main.sh

--- main.sh
+++ main.sh
@@ -3,9 +3,7 @@
 DEBIAN_FRONTEND=noninteractive
 
 # Clone Upstream
-mkdir -p ./src-pkg-name
-cp -rvf ./debian ./src-pkg-name/
-cd ./src-pkg-name/
+cd ./jasper-gtk-theme
 
 # Get build deps
 apt-get build-dep ./ -y
